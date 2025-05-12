dc5d54962386747503bc7d8648e1c0fe8e95b83e
Obadowski <vobadowski@gmail.com>
2025-05-12 17:09:18 -0300

    feat: Solve Exercise 14 (Fibonacci)
     - Uses the array solution
     - Personally, I don't like the proposed solution
       - Not because it is wrong. Just a personal PoW.

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,15 @@
-const fibonacci = function() {
+const fibonacci = function(order) {
+    let count = parseInt(order);
 
+    if (count < 0) return "OOPS";
+    if (count === 0) return 0;
+
+    let fibo = [1, 1];
+
+    for (let i = 2; i <= count; i++) {
+        fibo.push(fibo[i-1] + fibo[i-2]);
+    }
+    return fibo[count-1];
 };
 
 // Do not edit below this line
