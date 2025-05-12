04925d3d7007ea468eccc1140063b7318c1e06b8
Obadowski <vobadowski@gmail.com>
2025-05-12 15:28:47 -0300

    feat: Solve exercise 12
     - Created all functions.
     - Used reduce for array
     - Build a small loop for factorial (not recursive!)

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,25 +1,33 @@
-const add = function() {
-	
+const add = function(operand1, operand2) {
+	return operand1 + operand2;
 };
 
-const subtract = function() {
-	
+const subtract = function(operand1, operand2) {
+	return operand1 - operand2;
 };
 
-const sum = function() {
-	
+const sum = function(inputArray) {
+	return inputArray.reduce((sum, current) => sum + current, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(inputArray) {
+    return inputArray.reduce(
+        (operand, current) => operand * current, 1
+    );
 };
 
-const power = function() {
-	
+const power = function(base, exponent) {
+	return base ** exponent;
 };
 
-const factorial = function() {
-	
+const factorial = function(operand) {
+	let result = 1;
+    if (operand === 1) return result;
+    while(operand >= 1) {
+        result *= operand;
+        operand--;
+    }
+    return result;
 };
 
 // Do not edit below this line
