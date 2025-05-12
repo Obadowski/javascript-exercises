8eeae122da9cc154b040f693582287b0f1d845a2
Obadowski <vobadowski@gmail.com>
2025-05-12 16:02:26 -0300

    feat: Solve exercise 13
     - I somewhat disagree with ignoring spaces
     - This exercise was made to turn everyone mad with
     the reference issue of "reverse()"

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,12 @@
-const palindromes = function () {
+const palindromes = function (stringCandidate) {
+    const punctuation = [",", ".", "!", "?", ":", ";", " "];
+    const cleaned = [...stringCandidate.toLowerCase()]
+        .filter(c => !punctuation.includes(c));
+    
+    const invertedString = [...cleaned].reverse().join("");
 
+    if (cleaned.join("") === invertedString) return true;
+    return false;
 };
 
 // Do not edit below this line
