840738c064fb3970c54543829a94cfa504f2c21d
Obadowski <vobadowski@gmail.com>
2025-05-12 17:35:24 -0300

    feat: Solve Exercise 16 findTheOldest
     - My solution is straightforward. I use map then sort.
     - Their solution calculates the age then make a reduction,
     always storing the oldest.
     - My solution is obviously slower than theirs.
     - I am keeping this one due to efforts, but their solution are better.
     - I call for a sort, they manually do. Required details comparisons.

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,12 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    const peopleWithAge = people.map((person) => {
+        if(!person.yearOfBirth) person.yearOfBirth = (new Date()).getFullYear();
+        if(!person.yearOfDeath) person.yearOfDeath = (new Date()).getFullYear();
+        person.Age = person.yearOfDeath - person.yearOfBirth;
+        return person;
+    }).sort((a,b) => b.Age - a.Age);
 
+    return peopleWithAge[0];
 };
 
 // Do not edit below this line
